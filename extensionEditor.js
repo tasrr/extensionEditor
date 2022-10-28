add0bb54ef1bf7eed6f3e863d375fe56280c6fbb
tasrr <109923659+tasrr@users.noreply.github.com>
2022-10-28 23:12:56 +0900

    Add files via upload

---
+++ extensionEditor.js
@@ -0,0 +1,30 @@
+
+onUiUpdate(
+	function(){
+		
+		let id = "zzzEditor";
+		
+		let button = document.querySelector( "#" + id );
+
+		if( button === null ){
+
+			let btn = document.createElement( "button" );
+			btn.setAttribute( "id", id );
+			btn.appendChild( document.createTextNode( "OPEN ExtensionEditor" ) );
+			btn.disabled = true;
+			btn.addEventListener( "click", function() {
+				window.open( "file/extensions/extensionEditor/extensionEditor.html", null, "popup,top=100,left=100" );
+			});
+			document.body.insertBefore( btn, document.body.firstChild );
+
+			return;
+		}
+
+		if( gradioApp().querySelector( "#tab_txt2img" ) !== null ){
+			button.disabled = false;
+		}
+
+		return;
+
+	}
+);
